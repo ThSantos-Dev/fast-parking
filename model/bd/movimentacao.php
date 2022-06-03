b8f12b9e48f95ffd4bf9a78003a7a458ed5126d4
Thales Santos <tales_silvasantos02@hotmail.com>
2022-06-03 00:50:43 -0300

    CRUD de movimentação Ok;

--- model/bd/movimentacao.php
+++ model/bd/movimentacao.php
@@ -9,13 +9,15 @@
 // Import do arquivo responsável pela Conexão do BD 
 require_once('conexaoMySQL.php');
 
-/** Função responsável por criar Moviementação (entrada do veículo) 
-*   @author Thales Santos 
-*   @param Array $dados Informações da movimentação 
-*                       (Data/Hora entrada, id do veículo, id do cliente).
-*   @return Bool True se foi inserido, senão, false.
-* */
-function insertMovimentacao($dados) {
+/** 
+ *   Função responsável por criar Moviementação (entrada do veículo) 
+ *   @author Thales Santos 
+ *   @param Array $dados Informações da movimentação 
+ *                       (Data/Hora entrada, id do veículo, id do cliente).
+ *   @return Bool True se foi inserido, senão, false.
+ * */
+function insertMovimentacao($dados)
+{
     // Abrindo conexão com o BD
     $conexao = conexaoMySQL();
 
@@ -34,13 +36,12 @@ function insertMovimentacao($dados) {
                         '{$dados['horaEntrada']}',
                          {$dados['idVaga']},
                          {$dados['idVeiculo']}
-                )
-    ";
+                    )";
 
     // Validação para verificar se o Script está correto
-    if(mysqli_query($conexao, $sql)) {
+    if (mysqli_query($conexao, $sql)) {
         // Validação para verificar se houve inserção no BD
-        if(mysqli_affected_rows($conexao)) 
+        if (mysqli_affected_rows($conexao))
             $statusResposta = true;
     }
 
@@ -51,4 +52,307 @@ function insertMovimentacao($dados) {
     return $statusResposta;
 }
 
-?>
+/**
+ * Função reponsável por apagar uma Moviementação
+ * @param Int ID da movimentação 
+ * @return Bool True se foi apagado, senão, false.
+ */
+function deleteMovimentacao($id) {
+    // Abre conexão com o BD
+    $conexao = conexaoMySQL();
+
+    // Variável de ambiente
+    $statusResposta = (bool) false;
+
+    // Script SQL para apagar uma movimentação
+    $sql = "DELETE FROM tblMovimentacao
+                WHERE id = {$id}";
+    
+    // Validação para verificar se o Script SQL está correto
+    if(mysqli_query($conexao, $sql)) {
+        // Validação para verificar se o registro foi apagado
+        if(mysqli_affected_rows($conexao))
+            $statusResposta = true;
+    }
+
+    // Solicitando o fechamento da conexão com o BD
+    fecharConexaoMySQL($conexao);
+
+    // Retornando o status da solicitação
+    return $statusResposta;
+}
+
+
+/**
+ * Função responsável por alterar uma Moviementação (saída do veículo)
+ * @author Thales Santos
+ * @param Array $dados Informações da movimentação (Data/Hora saída e id da movimentação)
+ * @return Bool True se foi registrada a saída, senão, false.
+ */
+function updateMovimentacao($dados){
+    // Abrindo conexão com o BD
+    $conexao = conexaoMySQL();
+
+    // Variável de ambiente
+    $statusResposta = (bool) false;
+
+    // Script SQL para atualizar o registro da movimentação
+    $sql = "UPDATE tblMovimentacao SET 
+                dataSaida = {$dados['dataSaida']},
+                horaSaida = {$dados['horaSaida']}
+
+            WHERE id = {$dados['id']}";
+
+    // Validação para verificar se o Script SQL está correto
+    if (mysqli_query($conexao, $sql)) {
+        // Validação para verificar se houve a atualização do registro
+        if (mysqli_affected_rows($conexao))
+            $statusResposta = true;
+    }
+
+
+    // Solicitando o fehcamento da conexão com o BD
+    fecharConexaoMySQL($conexao);
+
+    // Retornando o status da solicitação
+    return $statusResposta;
+}
+
+
+/**
+ * Função responsável por listar todas as Movimentações 
+ * @param Void 
+ * @return Array Dados encontrados no BD
+ */
+function selectAllMovimentacoes() {
+       // Abrindo conexão com o BD
+       $conexao = conexaoMySQL();
+
+       // Script SQL para buscar uma Movimentação pelo ID
+       $sql = "SELECT 
+                   tblMovimentacao.id,
+                   tblMovimentacao.dataEntrada,
+                   tblMovimentacao.horaEntrada,
+                   tblMovimentacao.dataSaida,
+                   tblMovimentacao.horaSaida,
+   
+                   tblVeiculo.placa,
+                   tblVeiculo.fabricante,
+                   tblVeiculo.modelo,
+                   
+                   tblCor.nome AS cor,
+   
+                   tblTipoVeiculo.nome AS tipo,
+   
+                   tblCliente.nome AS cliente,
+                   tblCliente.telefone,
+   
+                   upper(concat_ws('-', tblPiso.nome, tblSetor.nome, tblCorredor.nome, tblVaga.codigo)) as sigla,
+   
+                   tblVaga.codigo AS codigo,
+       
+                   tblCorredor.nome AS corredor,
+   
+                   tblSetor.nome AS setor,
+   
+                   tblPiso.nome AS piso
+               
+               FROM tblMovimentacao
+                   INNER JOIN tblVeiculo 
+                       ON tblMovimentacao.idVeiculo = tblVeiculo.id
+                   INNER JOIN tblCor
+                       ON tblVeiculo.idCor = tblCor.id
+                   INNER JOIN tblTipoVeiculo
+                       ON tblVeiculo.idTipoVeiculo = tblTipoVeiculo.id
+   
+                   INNER JOIN tblCliente
+                       ON tblVeiculo.idCliente = tblCliente.id
+                   
+                   INNER JOIN tblVaga
+                       ON tblMovimentacao.idVaga = tblVaga.id
+   
+                   INNER JOIN tblCorredor
+                       ON tblVaga.idCorredor = tblCorredor.id
+                   INNER JOIN tblSetor
+                       ON tblCorredor.idSetor = tblSetor.id
+                   INNER JOIN tblPiso
+                       ON tblSetor.idPiso = tblPiso.id";
+   
+   
+       $resposta = mysqli_query($conexao, $sql);
+   
+       // Validação para verificar se houve retorno
+       if($resposta) {
+           // Convertendo os dados obtidos em  array
+           $contador = 0;
+           while($resultado = mysqli_fetch_assoc($resposta)) {
+               // Montando um array personalizado com os dados obtidos
+               $arraydados[$contador] = array(
+                   "id" => $resultado['id'],
+   
+                   "cliente" => array(
+                       "nome" => $resultado['cliente'],
+                       "telefone" => $resultado['telefone']
+                   ),
+   
+                   "veiculo" => array(
+                       "placa" => $resultado['placa'],
+                       "fabricante" => $resultado['fabricante'],
+                       "modelo" => $resultado['modelo'],
+                       "cor" => $resultado['cor'],
+                       "tipo" => $resultado['tipo']
+                   ),
+   
+                   "vaga" => array(
+                       "codigo" => $resultado['codigo'],
+                       "sigla" => $resultado['sigla'],
+   
+                       "localizacao" => array(
+                           "corredor" => $resultado['corredor'],
+                           "setor" => $resultado['setor'],
+                           "piso" => $resultado['piso']
+                       )
+                   ),
+   
+   
+                   "entrada" => array(
+                       "data" => $resultado['dataEntrada'],
+                       "horario" => $resultado['horaEntrada']
+                   ),
+                   "saida" => array(
+                       "data" => $resultado['dataSaida'],
+                       "horario" => $resultado['horaSaida']
+                   )
+               );
+
+            // Incrementando o contador para que não haja sobrescrita dos dados
+            $contador++;
+           }
+       }
+   
+   
+       // Solitando o fechamento da conexão com o BD
+       fecharConexaoMySQL($conexao);
+   
+       // Retornando os dados encontrados ou false
+       return isset($arraydados) ? $arraydados : false;
+}
+
+
+/**
+ * Função responsável por buscar uma Moviementação por ID
+ * @param Int ID da moviementação
+ * @return Array Dados encontrados na busca
+ */
+function selectByIdMovimentacao($id){
+    // Abrindo conexão com o BD
+    $conexao = conexaoMySQL();
+
+    // Script SQL para buscar uma Movimentação pelo ID
+    $sql = "SELECT 
+                tblMovimentacao.id,
+                tblMovimentacao.dataEntrada,
+                tblMovimentacao.horaEntrada,
+                tblMovimentacao.dataSaida,
+                tblMovimentacao.horaSaida,
+
+                tblVeiculo.placa,
+                tblVeiculo.fabricante,
+                tblVeiculo.modelo,
+                
+                tblCor.nome AS cor,
+
+                tblTipoVeiculo.nome AS tipo,
+
+                tblCliente.nome AS cliente,
+                tblCliente.telefone,
+
+                upper(concat_ws('-', tblPiso.nome, tblSetor.nome, tblCorredor.nome, tblVaga.codigo)) as sigla,
+
+                tblVaga.codigo AS codigo,
+    
+                tblCorredor.nome AS corredor,
+
+                tblSetor.nome AS setor,
+
+                tblPiso.nome AS piso
+            
+            FROM tblMovimentacao
+                INNER JOIN tblVeiculo 
+                    ON tblMovimentacao.idVeiculo = tblVeiculo.id
+                INNER JOIN tblCor
+                    ON tblVeiculo.idCor = tblCor.id
+                INNER JOIN tblTipoVeiculo
+                    ON tblVeiculo.idTipoVeiculo = tblTipoVeiculo.id
+
+                INNER JOIN tblCliente
+                    ON tblVeiculo.idCliente = tblCliente.id
+                
+                INNER JOIN tblVaga
+                    ON tblMovimentacao.idVaga = tblVaga.id
+
+                INNER JOIN tblCorredor
+                    ON tblVaga.idCorredor = tblCorredor.id
+                INNER JOIN tblSetor
+                    ON tblCorredor.idSetor = tblSetor.id
+                INNER JOIN tblPiso
+                    ON tblSetor.idPiso = tblPiso.id
+                
+            WHERE tblMovimentacao.id = {$id}";
+
+
+    $resposta = mysqli_query($conexao, $sql);
+
+    // Validação para verificar se houve retorno
+    if($resposta) {
+        // Convertendo os dados obtidos em  array
+        if($resultado = mysqli_fetch_assoc($resposta)) {
+            // Montando um array personalizado com os dados obtidos
+            $dados = array(
+                "id" => $resultado['id'],
+
+                "cliente" => array(
+                    "nome" => $resultado['cliente'],
+                    "telefone" => $resultado['telefone']
+                ),
+
+                "veiculo" => array(
+                    "placa" => $resultado['placa'],
+                    "fabricante" => $resultado['fabricante'],
+                    "modelo" => $resultado['modelo'],
+                    "cor" => $resultado['cor'],
+                    "tipo" => $resultado['tipo']
+                ),
+
+                "vaga" => array(
+                    "codigo" => $resultado['codigo'],
+                    "sigla" => $resultado['sigla'],
+
+                    "localizacao" => array(
+                        "corredor" => $resultado['corredor'],
+                        "setor" => $resultado['setor'],
+                        "piso" => $resultado['piso']
+                    )
+                ),
+
+
+                "entrada" => array(
+                    "data" => $resultado['dataEntrada'],
+                    "horario" => $resultado['horaEntrada']
+                ),
+                "saida" => array(
+                    "data" => $resultado['dataSaida'],
+                    "horario" => $resultado['horaSaida']
+                )
+            );
+        }
+    }
+
+
+    // Solitando o fechamento da conexão com o BD
+    fecharConexaoMySQL($conexao);
+
+    // Retornando os dados encontrados ou false
+    return isset($dados) ? $dados : false;
+}
+
